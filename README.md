2ae217c98d71665503835a3344bc4566c18c00f6
Jens Dibbern <jens.dibbern@gmail.com>
2020-03-22 19:34:03 +0100

    add Python version

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ You can now use that command in any new service definition.
 ## PhantomJS instead of Chrome
 
 Selenium 4 dropped PhantomJS support so this tool uses Chrome (widely available and standard browser for a majority of people). 
-If you switch back to the latest stable Selenium 3 release and change a few lines, you can use PhantomJS as Chrome replacement. But you will lose the mobile device emulation option. 
+If you switch back to the latest stable Selenium 3 release and change a few lines, you can use PhantomJS as Chrome replacement. But you will lose the mobile device emulation option. But PhantomJS is deprecated and you should use Chrome in headless mode.
 
 ```
 		WebDriverManager.phantomjs().setup();
@@ -53,4 +53,25 @@ If you switch back to the latest stable Selenium 3 release and change a few line
 	        WebDriverManager.phantomjs().getBinaryPath()
 	    );		
 		internalDriver = new PhantomJSDriver(capabilities); 
+```
+
+## Python version
+
+There is a minimal version of this rewritten in Python in the plugins directory that even runs on an old Raspberry Pi 2 with Raspbian Buster. You will need the following prerequisites:   
+```
+sudo apt install python-pip
+sudo pip install selenium
+sudo apt-get install chromium chromium-chromedriver
+```
+
+Then copy the SeleniumElementCheck.py file to /usr/lib/nagios/plugins/ and save your configuration file as /etc/nagios-plugins/config/selenium-checks.xml.
+
+The commands.cfg for Nagios 4 on Raspbian needs the following addition: 
+```
+# define selenium check command
+define command{
+        command_name    check-selenium
+        command_line    /usr/bin/python /usr/lib/nagios/plugins/SeleniumElementCheck.py /etc/nagios-plugins/config/selenium-checks.xml 2>/var/log/nagios4/selenium.log
+        }
+
 ```
---
+++ plugin/SeleniumElementCheck.py
@@ -0,0 +1,87 @@
+# -----------------------------------------------------------
+# Checks website elements by CSS selector using Selenium
+#
+# (C) 2020 Jens Dibbern
+# Released under public domain
+# -----------------------------------------------------------
+import sys
+import xml.etree.ElementTree as ET
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, SessionNotCreatedException
+from selenium.webdriver.chrome.options import Options
+
+# prepare Chrome options for headless mode and avoid issues running in background
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--disable-features=NetworkService')
+options.add_argument('--disable-features=VizDisplayCompositor')
+
+# setup webdriver (executable for chromedriver must be available in path
+try:
+    driver = webdriver.Chrome(executable_path='d:\develop\chromedriver.exe', options=options)
+except SessionNotCreatedException:
+    print('UNKNOWN: could not init webdriver')
+    sys.exit(3)
+
+# check command line arguments
+if len(sys.argv) != 2:
+    print('UNKNOWN: command uses just one argument for config file')
+    sys.exit(3)
+
+tree = ET.parse(sys.argv[1])
+root = tree.getroot()
+
+warn_count = 0
+error_count = 0
+messages = ""
+
+# iterate through all the check subtrees in XML document
+for check in root.iter('check'):
+
+    # open website
+    url = check.attrib['url']
+    driver.get(url)
+
+    # iterate through the elements to check
+    for element in check:
+        selector = element.attrib['selector']
+        level = element.attrib['level']
+        try:
+            # check for element existence
+            elem = driver.find_element_by_css_selector(selector)
+            if elem.tag_name == 'img':
+                # check if image is loaded
+                assert isinstance(elem, object)
+                img_loaded = driver.execute_script(
+                    'return arguments[0].complete && typeof arguments[0].naturalWidth != "undefined" && arguments[0].naturalWidth > 0',
+                    elem)
+                if not img_loaded:
+                    messages += 'image not loaded: %s - %s\n' % (url, selector)
+                    if level == 'warn':
+                        warn_count += 1
+                    else:
+                        error_count += 1
+        except NoSuchElementException:
+            messages += 'failed: %s - %s\n' % (url, selector)
+            if level == 'warn':
+                warn_count += 1
+            else:
+                error_count += 1
+
+driver.quit()
+
+if error_count > 0:
+    print('CRITICAL: There were errors')
+    print(messages)
+    sys.exit(2)
+
+if warn_count > 0:
+    print('WARN: There were warnings')
+    print(messages)
+    sys.exit(1)
+
+print('OK: Check successful')
+sys.exit(0)
